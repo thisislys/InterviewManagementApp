6fc011687293c935fb4fb8b6c587a6f8e1b643e8
lylylyly <lylylyly163.com>
2019-03-21 21:21:17 +0800

    添加面试

--- sign/src/api/index.js
+++ sign/src/api/index.js
@@ -1,8 +1,14 @@
 import request from '@/utils/request';
 
 // 登陆接口
-export let login = code=>{
-  return request.post('http://169.254.12.68:7001/user/code2session', {
+export let login = code => {
+  return request.post('http://123.206.55.50:7001/user/code2session', {
     code
   })
+}
+// 添加面试
+export let addSign = params => {
+  return request.post('http://123.206.55.50:7001/sign',
+    params
+  )
 }
--- sign/src/store/modules/interview.js
+++ sign/src/store/modules/interview.js
@@ -1,3 +1,6 @@
+import {
+  addSign
+} from '@/api/index'
 const state = {
   list: [],
   current: {
@@ -9,20 +12,26 @@ const state = {
 }
 
 const mutations = {
-  updateState(state, payload){
-    state.current = {...state.current, ...payload};
+  updateState(state, payload) {
+    state.current = {
+      ...state.current,
+      ...payload
+    };
   }
 }
 
-
 const actions = {
-  submit(state, payload){
-    console.log('payload...', payload);
-    return new Promise((resolve, reject)=>{
-      setTimeout(function(){
-        resolve();
-      }, 5000);
+  async submit(state, {...payload}) {
+    return new Promise(async(resolve, reject) => {
+      //填充经纬度
+      payload.latitude = payload.address.location.lat;
+      payload.longitude = payload.address.location.lng;
+      //序列化地址
+      payload.address = JSON.stringify(payload.address);
+      let result = await addSign(payload);
+      resolve(result)
     })
+
   }
 }
 
